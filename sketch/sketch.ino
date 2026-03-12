7eb463f1693ec797ce379565c4fa87390e275418
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-12 08:13:09 +0100

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,1 +1,50 @@
+#include <Arduino.h>
+#include <Arduino_RouterBridge.h>
+#include <Modulino.h>
 
+ModulinoButtons buttons;
+
+void requestTime(const char* buttonName);
+
+void setup() {
+  Bridge.begin();
+  Monitor.begin();
+
+  Modulino.begin();
+  buttons.begin();
+
+  Monitor.println("MCU ready");
+}
+
+void loop() {
+  if (buttons.update()) {
+
+    if (buttons.isPressed('A')) {
+      requestTime("A");
+    }
+
+    if (buttons.isPressed('B')) {
+      requestTime("B");
+    }
+
+    if (buttons.isPressed('C')) {
+      requestTime("C");
+    }
+  }
+
+  delay(20);
+}
+
+void requestTime(const char* buttonName) {
+  Monitor.println(String("Button ") + buttonName + " pressed");
+
+  String timeString;
+  RpcCall rpc = Bridge.call("get_system_time");
+
+  if (rpc.result(timeString)) {
+    Monitor.println("Time received from MPU: " + timeString);
+  } else {
+    Monitor.println("RPC error code: " + String(rpc.getErrorCode()));
+    Monitor.println("RPC error message: " + rpc.getErrorMessage());
+  }
+}
