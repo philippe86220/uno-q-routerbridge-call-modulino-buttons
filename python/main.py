3d6db70a50afb6dc7be73a9569fc5c6d39379c6b
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-12 08:14:31 +0100

    Add system time retrieval and integration with Bridge

--- python/main.py
+++ python/main.py
@@ -1,1 +1,15 @@
+from arduino.app_utils import App, Bridge
+import datetime
 
+def get_system_time():
+
+    now = datetime.datetime.now()
+
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == "__main__":
+
+    Bridge.provide("get_system_time", get_system_time)
+
+    App.run()
